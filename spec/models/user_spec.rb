79de5b38003f53611c7d7421044fc10f9fe4a8d0
Brenden Thornton <thorntonbrenden@gmail.com>
2019-07-31 19:40:16 -0600

    created some basic tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    User.create(
+      :firstname => "Benjamin",
+      :lastword => "Thornton",
+      :email => "benji@dog.com",
+      :password => "dogpoop"
+    )
+  }
+
+  it "is valid with a firstname, lastname, email, and password" do
+    expect(user).to be_valid
+  end
+
+  # it 'is not valid when missing required attributes' do
+  #   user = User.new
+  #   expect(user).not_to be_valid
+  # end
+
+  it "is not valid without a password" do
+    expect(User.new(firstname: "Name")).not_to be_valid
+  end
+
+  it 'should have many fishtanks' do
+    association = User.reflect_on_association(:fishtanks)
+    expect(association).to eq(:has_many)
+  end
+
 end
